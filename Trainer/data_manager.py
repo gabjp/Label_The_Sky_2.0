23506cda0ee23bf02dd7d2baf058ad1d08bab0dd
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-04 15:04:34 -0300

    Implement Trainer

---
+++ Trainer/data_manager.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+CLF_READY_FOLDER = '../Data/ready/clf/'
+UNL_READY_FOLDER = '../Data/ready/unl/'
+CLF_TYPES = ['image', 'tabular', 'wiseflags', 'class']
+UNL_TYPES = ['image', 'tabular']
+SPLITS = ['train', 'val', 'test']
+
+def load_data(ds_name, is_clf):
+    output = {}
+    types = CLF_TYPES if is_clf else UNL_TYPES
+    for split in SPLITS:
+        for type in types:
+            output[type +'_' +split] = np.load(CLF_READY_FOLDER + ds_name + '_' +type +'_' +split +'.npy')
+    return output
+
+    
---
+++ Trainer/sky_classifier.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+from sklearn.metrics import classification_report
+
+MODELS = ['vgg16', 'RF']
+CLASS_NAMES = ['QSO', 'STAR', 'GALAXY']
+PRETEXT_OUTPUT_TYPES = ['magnitudes', 'images', None]
+SEED = 2
+OUT_DIR = "../outs/" # This assumes that the program will be run from scripts dir
+
+class SkyClassifier:
+    def __init__(self, model, model_name, wise, pretext_output=None, save=True):
+        if model not in MODELS: raise ValueError(f"{model} not in {MODELS}")
+        self.model_type = model
+
+        if pretext_output not in PRETEXT_OUTPUT_TYPES: 
+            raise ValueError(f"{pretext_output} not in {PRETEXT_OUTPUT_TYPES}")
+        self.pretext_output = pretext_output
+
+        self.wise = wise
+        self.save = save
+        self.model_name = model_name
+        self.model_folder = OUT_DIR+self.model_name + '/'
+
+        if self.save:
+            os.mkdir(OUT_DIR+self.model_name)
+
+    def build_model(self, **kwargs):
+        if self.model_type == "RF":
+            self.model = RandomForestClassifier(random_state = SEED, **kwargs)
+            
+
+        elif self.model_type == "vgg16":
+            pass
+
+    def finetune(self):
+        pass
+
+    def pretrain(self):
+        pass
+
+    def train(self, X, y, notes):
+        if self.model_type == "RF":
+            self.model.fit(X, y=y)
+
+            if self.save: 
+                joblib.dump(self.model, self.model_folder + self.model_name + '.sav')
+                with open(self.model_folder + self.model_name + '.log') as log:
+                    log.write(f"""Model: {self.model_type}\n
+                              Model Name: {self.model_name}\n
+                              Wise: {self.wise}\n
+                              {self.model.get_params()}\n
+                              Notes: {notes}""")
+
+        elif self.model_type == "vgg16":
+            pass
+
+
+    def load_model(self):
+        if self.model_type == "RF":
+            self.model = joblib.load(self.model_folder + self.model_name + '.sav')
+        
+        elif self.model_type == "vgg16":
+            pass
+
+    def eval(self, X, y, ds_name, wise_flags = None):
+        if self.model_type == "RF":
+            with open(self.model_folder + self.model_name + '_' + ds_name + '.results') as results:
+                    pred = self.model.predict(X)
+                    total = classification_report(y, pred, digits = 6, target_names = CLASS_NAMES)
+                    with_wise = classification_report(y[wise_flags], pred[wise_flags], digits = 6, target_names = CLASS_NAMES) if wise_flags != None else None
+                    no_wise = classification_report(y[~wise_flags], pred[~wise_flags], digits = 6, target_names = CLASS_NAMES) if wise_flags != None else None
+
+                    output = f"""Model: {self.model_type}\n
+                              Model Name: {self.model_name}\n
+                              Wise: {self.wise}\n
+                              Data set name: {ds_name}\n
+                              Total:\n
+                              {total}
+                              With Wise:\n
+                              {with_wise}
+                              Without Wise:\n
+                              {no_wise}
+                              """
+                    
+                    if self.save:
+                        results.write(output)
+                    else:
+                        print(output)
+            
+
+
+        elif self.model_type == "vgg16":
+            pass
+        
+    def predict(self):
+        pass
+
+    def predict_proba(self):
+        pass
