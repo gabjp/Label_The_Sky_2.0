e8afc7b0194737be0267440c55217f1702beee0e
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-04 15:47:41 -0300

    fix ready folder

--- Trainer/data_manager.py
+++ Trainer/data_manager.py
@@ -1,12 +1,12 @@
 import numpy as np
 import os
 
-CLF_READY_FOLDER = f"Data/ready/clf/"
-UNL_READY_FOLDER = f"Data/ready/unl/"
+CLF_READY_FOLDER = os.path.abspath("..") + "/Data/ready/clf/"
+UNL_READY_FOLDER = os.path.abspath("..") + "/Data/ready/unl/"
 CLF_TYPES = ['image', 'tabular', 'wiseflags', 'class']
 UNL_TYPES = ['image', 'tabular']
 SPLITS = ['train', 'val', 'test']
-print(os.path.abspath("."))
+
 print(os.path.abspath(".."))
 
 def load_data(ds_name, is_clf):
@@ -14,7 +14,8 @@ def load_data(ds_name, is_clf):
     types = CLF_TYPES if is_clf else UNL_TYPES
     for split in SPLITS:
         for type in types:
-            output[type +'_' +split] = np.load(CLF_READY_FOLDER + ds_name + '_' +type +'_' +split +'.npy')
+            with open(CLF_READY_FOLDER + ds_name + '_' +type +'_' +split +'.npy', "r")as file:
+                output[type +'_' +split] = np.load(file)
     return output
 
     
