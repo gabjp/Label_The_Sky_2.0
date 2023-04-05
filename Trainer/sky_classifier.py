b36a572f3b1177fbc6436f8772f24cbb092d41ad
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-05 13:55:39 -0300

    Fix RF_Generator.py

--- Trainer/sky_classifier.py
+++ Trainer/sky_classifier.py
@@ -71,6 +71,7 @@ class SkyClassifier:
             with open(self.model_folder + self.model_name + '_' + ds_name + '.results', 'w') as results:
                     pred = self.model.predict(X)
                     total = classification_report(y, pred, digits = 6, target_names = CLASS_NAMES)
+                    print(wise_flags)
                     with_wise = classification_report(y[wise_flags,:], pred[wise_flags,:], digits = 6, target_names = CLASS_NAMES) if wise_flags != None else None
                     no_wise = classification_report(y[~wise_flags,:], pred[~wise_flags,:], digits = 6, target_names = CLASS_NAMES) if wise_flags != None else None
 
