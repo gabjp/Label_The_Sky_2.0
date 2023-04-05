2b3f5265a22b3c146f4e507fed5022bd3d6306f7
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-05 13:00:09 -0300

    Fix RF_Generator.py

--- scripts/RF_Generator.py
+++ scripts/RF_Generator.py
@@ -13,7 +13,7 @@ def main():
     print("Generating Unified RF", flush=True)
     #Generate unified RF:
     unified_rf = SkyClassifier('RF', "Unified_RF", True)
-    unified_rf.build_model(n_estimator=100, bootstrap=False)
+    unified_rf.build_model(n_estimators=100, bootstrap=False)
     unified_rf.train(data["tabular_train"], data["class_train"], " ")
     unified_rf.eval(data["tabular_val"], data["class_val"], "clf_90_5_5_val", wise_flags = data['wiseflags_val'])
     unified_rf.eval(data["tabular_test"], data["class_test"], "clf_90_5_5_test", wise_flags=data['wiseflags_test'])
@@ -22,7 +22,7 @@ def main():
     print("Generating Without Wise RF", flush=True)
     #Generate Without Wise RF:
     nowise_rf = SkyClassifier('RF', "Without_Wise_RF", True)
-    nowise_rf.build_model(n_estimator=100, bootstrap=False)
+    nowise_rf.build_model(n_estimators=100, bootstrap=False)
     nowise_rf.train(data["tabular_train"][:,0:16], data["class_train"], "This model only uses SPLUS features")
     nowise_rf.eval(data["tabular_val"][:,0:16], data["class_val"], "clf_90_5_5_val", wise_flags = data['wiseflags_val'])
     nowise_rf.eval(data["tabular_test"][:,0:16], data["class_test"], "clf_90_5_5_test", wise_flags=data['wiseflags_test'])
@@ -31,7 +31,7 @@ def main():
     print("Generating Only Wise RF", flush=True)
     #Generate Only Wise RF:
     onlywise_rf = SkyClassifier('RF', "Without_Wise_RF", True)
-    onlywise_rf.build_model(n_estimator=100, bootstrap=False)
+    onlywise_rf.build_model(n_estimators=100, bootstrap=False)
     nowise_rf.train(data["tabular_train"][data['wiseflags_train'],:], data["class_train"][data['wiseflags_train'],:], "This model only uses objects with wise magnites")
     nowise_rf.eval(data["tabular_val"][data['wiseflags_val']], data["class_val"][data['wiseflags_val']], "clf_90_5_5_val_withwise")
     nowise_rf.eval(data["tabular_test"][data['wiseflags_test']], data["class_test"][data['wiseflags_test']], "clf_90_5_5_test_withwise")
