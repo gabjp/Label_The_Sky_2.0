53fb2b39247e99067f62199323e3937ee702bb7e
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-07 16:46:55 -0300

    fix from scratch vgg

--- scripts/from_scratch.py
+++ scripts/from_scratch.py
@@ -14,16 +14,18 @@ def main():
     wise_val, wise_test = data['wiseflags_val'], data['wiseflags_test'] 
     print("Loaded Data", flush=True)
 
-    for lr in [5*1e-4,1e-4,1e-5]:
-        for l2 in [0,0.0007,0.001]:
-            for dpout in [0,0.2, 0.5]:
-                print(f"vgg16_from_scratch_lr:{lr}_l2:{l2}_dropout:{dpout}")
-                model = SkyClassifier(f"vgg16", f"vgg16_from_scratch_lr:{lr}_l2:{l2}_dropout:{dpout}", False)
-                opt = tf.keras.optimizers.Adam(learning_rate=lr)
-                model.build_model(l2=l2, dropout=dpout, opt=opt)
-                model.train(X, y, X_val=X_val, y_val=y_val, notes = f"lr: {lr}, l2: {l2}, dropout: {dpout}", epochs=100)
-                model.eval(X_val, y_val, "clf_90_5_5_val", wise_flags=wise_val)
-                model.eval(X_test, y_test, "clf_90_5_5_test", wise_flags=wise_test)
+    for bsize in [32,64]:
+        for lr in [1e-4,1e-5]:
+            for l2 in [0.0007,0]:
+                for dpout in [0.2,0,0.5]:
+
+                    print(f"vgg16_from_scratch_lr:{lr}_l2:{l2}_dropout:{dpout}_bsize:{bsize}", flush=True)
+                    model = SkyClassifier(f"vgg16", f"vgg16_from_scratch_lr:{lr}_l2:{l2}_dropout:{dpout}_bsize:{bsize}", False)
+                    opt = tf.keras.optimizers.Adam(learning_rate=lr)
+                    model.build_model(l2=l2, dropout=dpout, opt=opt)
+                    model.train(X, y, X_val=X_val, y_val=y_val, notes = f"lr: {lr}, l2: {l2}, dropout: {dpout}", epochs=100, batch_size=bsize)
+                    model.eval(X_val, y_val, "clf_90_5_5_val", wise_flags=wise_val)
+                    model.eval(X_test, y_test, "clf_90_5_5_test", wise_flags=wise_test)
 
 if __name__ == "__main__":
     main()
