1e9dd9d5d5a88db421b2a29f51fa1aab19d190b7
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-07 21:29:25 -0300

    fix bash to train models

--- scripts/from_scratch_ht.sh
+++ scripts/from_scratch_ht.sh
@@ -4,7 +4,7 @@ for bsize in 32 64; do
     for lr in 0.0001 0.00001; do
         for l2 in 0 0.0007; do
             for dpout in 0.2 0 0.5; do
-                nohup python from_scratch.py $bsize $lr $l2 $dpout & ls
+                nohup python from_scratch.py $bsize $lr $l2 $dpout 
             done
         done
     done
