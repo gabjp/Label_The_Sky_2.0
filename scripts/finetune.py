369c00bd1dce34c11b06f6678afe4deb40d2bd0a
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-04-16 19:00:20 -0300

    save decoder weights

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -14,7 +14,7 @@ def main():
     wise_val, wise_test = data['wiseflags_val'], data['wiseflags_test'] 
     print("Loaded Data", flush=True)
 
-    weights_path = "../outs/vgg16_pretrain_img_lr_0.0001_l2_0.0/decoder.h5"
+    weights_path = "../outs/vgg16_pretrain_img_lr_0.0001_l2_0.0/decoder"
     w_lr = float(sys.argv[1])
     f_lr = float(sys.argv[2])
     l2 = float(sys.argv[3])
