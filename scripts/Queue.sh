b052bd2d1e7d4e88d24e9de5ab06f288aeb27ef2
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-05-05 18:45:46 -0300

    queue finetune mags

--- scripts/Queue.sh
+++ scripts/Queue.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-for lr in 0.0001 0.00001 0.00005; do
-    for dpout in 0 0.3 0.5; do
-        python mag_pretrain.py $lr 0 $dpout
+for lrw in 0.0001 0.00001; do
+    for lr in 0.00001 0.000001; do
+        for dpout in 0.3 0.5; do
+            for l2 in 0 0.0007; do
+                python finetune.py $lrw $lr $l2 $dpout
+            done
+        done
     done
 done
-
-
 
 
     
--- scripts/finetune.py
+++ scripts/finetune.py
@@ -14,7 +14,7 @@ def main():
     wise_val, wise_test = data['wiseflags_val'], data['wiseflags_test'] 
     print("Loaded Data", flush=True)
 
-    weights_path = "../outs/vgg16_4x4_pretrain_img_lr_0.0001_l2_0.0/decoder"
+    weights_path = "../outs/vgg16mod_pretrain_mags_lr_1e-05_l2_0.0_dt_0.0/vgg16mod_pretrain_mags_lr_1e-05_l2_0.0_dt_0.0"
     w_lr = float(sys.argv[1])
     f_lr = float(sys.argv[2])
     l2 = float(sys.argv[3])
