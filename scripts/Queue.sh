529d49b8389b9d8071735f41a511668b9fd2dec3
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-05-14 20:02:10 -0300

    clean from scratch

--- scripts/Queue.sh
+++ scripts/Queue.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for lrw in 0.0001 0.00001; do
-    for lr in 0.00001 0.000001; do
-        for dpout in 0.3 0.5; do
-            for l2 in 0 0.0007; do
-                python finetune.py $lrw $lr $l2 $dpout
-            done
+
+for lr in 0.00001 0.0001; do
+    for dpout in 0.3 0.5; do
+        for l2 in 0 0.0007; do
+            python from_scratch.py $lr $l2 $dpout
         done
     done
 done
+
 
 
     
--- scripts/from_scratch.py
+++ scripts/from_scratch.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 def main():
     print("Loading Data", flush=True)
     ds_name = 'clf_90_5_5'
-    data = load_data(ds_name, True)
+    data = load_data(ds_name, True, True)
     X,y = data['images_train'], data['class_train']
     X_val,y_val = data['images_val'], data['class_val']
     X_test,y_test = data['images_test'], data['class_test']
@@ -17,7 +17,7 @@ def main():
     lr = float(sys.argv[1])
     l2 = float(sys.argv[2])
     dpout = float(sys.argv[3])
-    model_name = f"vgg16mod_from_scratch_lr_{lr}_l2_{l2}_dt_{dpout}"
+    model_name = f"vgg16clean_from_scratch_lr_{lr}_l2_{l2}_dt_{dpout}"
 
     print(model_name, flush=True)
     model = SkyClassifier(f"vgg16", model_name, False)
