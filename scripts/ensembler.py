fe769e7d7201f07fd3588b3fe3eb4e3bcbb871cc
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-05-23 21:29:01 -0300

    ensember

--- scripts/ensembler.py
+++ scripts/ensembler.py
@@ -48,8 +48,6 @@ def generate_data():
         vgg.load_model(weights_path= weights_path, finetune=True)
         opt = tf.keras.optimizers.Adam(learning_rate=0.00001)
 
-        for layer in vgg.model.layers: print(layer.get_config(), layer.get_weights()[:100]) #debug
-
         vgg.finetune(data["images_train"][train_index], data["class_train"][train_index],
                       data["images_val"], data["class_val"], opt, f_epochs=1)
         vgg.load_model()
